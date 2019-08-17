2fcdf8425801d2d26850ddb6ca2b6826c6223ad8
hadeelfleifel <51743597+hadeelfleifel@users.noreply.github.com>
2019-08-17 23:29:28 +0300

    Add files via upload

---
+++ build/precache-manifest.bcd258b25c66202c9ab0570a6afee552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3698a4641914dcfa8646e2978c2d45d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2661bd898883a4e7f4e3",
+    "url": "/static/css/main.e6f9a08a.chunk.css"
+  },
+  {
+    "revision": "6cda36b6e3e24619cddc",
+    "url": "/static/js/2.4d56fab7.chunk.js"
+  },
+  {
+    "revision": "2661bd898883a4e7f4e3",
+    "url": "/static/js/main.32019128.chunk.js"
+  },
+  {
+    "revision": "8218883609020070a601",
+    "url": "/static/js/runtime~main.3b7de09a.js"
+  }
+]);
